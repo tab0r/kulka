c87091d8becb42d25f47adc42085e9bd19e0d251
Tabor H <thetabor@gmail.com>
2017-05-27 23:12:32 -0600

    Sensor polls and conversion working! Next will be actual streaming

--- examples/data_stream.py
+++ examples/data_stream.py
@@ -11,19 +11,38 @@ def main():
     addr = '68:86:E7:06:FD:1D'
     with Kulka(addr) as kulka:
         kulka.set_inactivity_timeout(3600)
-
-        for _ in range(10):
-            kulka.set_rgb(0, 0xFF, 0)
-            time.sleep(0.1)
-            kulka.set_rgb(0, 0, 0)
-            time.sleep(0.1)
-            kulka.roll(10, 0)
-            kulka.read_locator()
-            data_list = []
-            for kbyte in kulka.data_stream()[2]:
-                data_list.append(kbyte)
-                #print(int.from_bytes(kbyte, byteorder='big'))
-            print(data_list)
+        for i in range(5):
+            direction = input('What direction?\n')
+            if int(direction) == -1:
+                break
+            speed = input('What speed?\n')
+            kulka.roll(int(speed), int(direction))
+            for i in range(4):
+                kulka.set_rgb(0, 0, 0xFF)
+                time.sleep(0.1)
+                kulka.set_rgb(0, 0, 0)
+                time.sleep(0.1)
+                kulka.set_rgb(0xFF, 0, 0)
+                # kulka.roll(0, 180 * i % 360)
+                kulka.read_locator()
+                data = kulka.data_stream()
+                print("Raw output:\n", data[2])
+                data_list = []
+                for kbyte in data[2]:
+                    data_list.append(kbyte)
+                    # print(type(kbyte))
+                output = dict()
+                output['xpos'] = 256*data_list[0] + data_list[1]
+                output['ypos'] = 256*data_list[2] + data_list[3]
+                output['xvel'] = 256*data_list[4] + data_list[5]
+                output['yvel'] = 256*data_list[6] + data_list[7]
+                for i, k in enumerate(output):
+                    # print(k, output[k])
+                    if output[k] > 32767:
+                        output[k] -= 65536
+                # output['vel'] = data_list[8] + data_list[9]
+                print(output)
+                time.sleep(0.1)
         kulka.sleep()
 
 
