a2f9ed4197397b660c38e53b1f9cafbfc6eeb264
Tabor H <thetabor@gmail.com>
2017-06-08 14:13:01 -0600

    updates to make examples prettier

--- examples/data_poll.py
+++ examples/data_poll.py
@@ -7,6 +7,23 @@ sys.path.append(os.path.abspath("../"))
 from kulka import Kulka
 from random import randint
 
+def parse_locator(data):
+    data_list = []
+    for kbyte in data[2]:
+        data_list.append(kbyte)
+        # print(type(kbyte))
+    output = dict()
+    output['xpos'] = 256*data_list[0] + data_list[1]
+    output['ypos'] = 256*data_list[2] + data_list[3]
+    output['xvel'] = 256*data_list[4] + data_list[5]
+    output['yvel'] = 256*data_list[6] + data_list[7]
+    for i, k in enumerate(output):
+        # print(k, output[k])
+        if output[k] > 32767:
+            output[k] -= 65536
+    output['sog'] = data_list[8] + data_list[9]
+    return output
+
 def main():
     addr = '68:86:E7:06:FD:1D'
     with Kulka(addr) as kulka:
@@ -26,20 +43,7 @@ def main():
                 kulka.read_locator()
                 data = kulka.data_poll()
                 # print("Raw output:\n", data[2])
-                data_list = []
-                for kbyte in data[2]:
-                    data_list.append(kbyte)
-                    # print(type(kbyte))
-                output = dict()
-                output['xpos'] = 256*data_list[0] + data_list[1]
-                output['ypos'] = 256*data_list[2] + data_list[3]
-                output['xvel'] = 256*data_list[4] + data_list[5]
-                output['yvel'] = 256*data_list[6] + data_list[7]
-                for i, k in enumerate(output):
-                    # print(k, output[k])
-                    if output[k] > 32767:
-                        output[k] -= 65536
-                output['sog'] = data_list[8] + data_list[9]
+                output = parse_locator(data)
                 print(output)
         kulka.sleep()
 
--- examples/multi_sphero.py
+++ examples/multi_sphero.py
@@ -21,12 +21,23 @@ def main():
                 kulka1.set_inactivity_timeout(30)
                 kulka2.set_inactivity_timeout(30)
 
-                for _ in repeat(None, 20):
-                    # kulka0.roll(randint(30, 50), randint(0, 359))
-                    # kulka1.roll(randint(30, 50), randint(0, 359))
-                    # kulka2.roll(randint(30, 50), randint(0, 359))
+                kulka0.set_rgb(0, 0, 0xFF)
+                kulka1.set_rgb(0, 0xFF, 0)
+                kulka2.set_rgb(0xFF, 0, 0)
+                time.sleep(20)
+                kulka0.set_rgb(0xFF, 0xFF, 0xFF)
+                kulka1.set_rgb(0xFF, 0xFF, 0xFF)
+                kulka2.set_rgb(0xFF, 0xFF, 0xFF)
+                time.sleep(20)
 
-                    time.sleep(1)
+                # for i in range(20):
+                #     kulka0.roll(15, i*45 % 360)
+                #     kulka1.roll(15, i*45 % 360)
+                #     kulka2.roll(15, i*45 % 360)
+                #     time.sleep(3)
+                #     kulka0.roll(0, i*45 % 360)
+                #     kulka1.roll(0, i*45 % 360)
+                #     kulka2.roll(0, i*45 % 360)
 
                 kulka0.sleep()
                 kulka1.sleep()
